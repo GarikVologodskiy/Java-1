dfcf8bcae6359c0391d095375f11e84e080c0a16
GarikVologodskiy <Bavaria22001>
2019-04-23 06:29:49 +0300

    About HW

---
+++ src/ru/sapphire/courses/pcg1/Table1.java
@@ -0,0 +1,9 @@
+package ru.sapphire.courses.pcg1;
+
+import ru.sapphire.courses.Table;
+
+public class Table1 {
+    public static void main(String... __) {
+        Table.main(new String[]{});
+       }
+}
